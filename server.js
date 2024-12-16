10550c89945b70c71ed761be7e37f5a0ff6e9173
Aniket Shisodia <aniketshisodia7@gmail.com>
2024-12-17 00:10:04 +0530

    Added MongoDB connection and updated backend setup

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<DATABASE_PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB Connection');
+    // .catch(err => console.log(err));
+});
 
 app.listen(8000, () => {
     console.log('Server  is runnning at port 8000 ');
