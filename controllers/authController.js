87e7cf98e574c8e0078b2b92d5f048131d606025
Aniket Shisodia <aniketshisodia7@gmail.com>
2024-12-25 15:43:36 +0530

    Implemented JSON webtokens to login and signup

---
+++ controllers/authController.js
@@ -0,0 +1,47 @@
+// import jwt
+const jwt = require('jsonwebtoken');
+
+// Generate jwt tokens
+exports.generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+};
+
+
+// middleware function to protect route
+exports.protectRoute = (req, res, next) => {
+
+    // Extract token from 'Authorization Header'  (Bearer token format)
+
+    // Authorization: Bearer abc123xyz456
+    // Authorization is the header name.
+    // Bearer is the type of token being used.
+    // abc123xyz456 is the actual token(an encoded string, typically a JWT token in the case of modern web apps).
+
+    const token = req.headers.authorization?.split(' ')[1];
+    // authorization? ----> optional chaining , if authorization is null
+
+    if (!token) {
+        return res.status(401).json({ message: 'You are not logged in !' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: 'Invalid Token !' });
+        }
+        // Attach user id to request object
+        // like roles , mail , id for routes
+        req.user = decoded;
+        next();
+    });
+
+    // verify the token using the jwt secret key
+    // What is decoded ?
+    //     jwt.verify(token, secret, callback) :
+    //     This method is used to verify and decode a JWT token.
+    //     It checks if the token is valid(not tampered with, correctly signed, and not expired).
+    //     If the token is valid, the payload of the token is returned to the callback function in
+    //     the decoded parameter.The payload contains information encoded into the JWT, like the
+    //     user’s ID, roles, permissions, etc.
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/userControllers');
 const router = express.Router();
+const authController = require('../controllers/authController');
 
 //define signup route
-router.post('/signup', userController.signUp);
-router.post('/login', userController.login);
+router.post('/signup', authController.protectRoute, userController.signUp);
+router.post('/login', authController.protectRoute, userController.login);
 
 module.exports = router;
